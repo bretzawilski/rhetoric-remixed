c9dcccf002d3220df97907c3363abc2b475b8540
Bret Zawilski <bret.zawilski@gmail.com>
2021-07-06 18:55:44 +0100

    Trying for flash

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pageBody = document.getElementById("container");
+const pageBody = document.getElementById("rhetoric");
 
 let rhetoric = "";
 
@@ -21,6 +21,9 @@ function getRandomInt(max) {
 function printLine() {
   let line = getRandomInt(rhetoric.length);
   let addLine = document.createElement("p");
+  if (pageBody.childElementCount === 4) {
+    pageBody.firstElementChild.remove();
+  }
   addLine.innerText = rhetoric[line];
   pageBody.appendChild(addLine);
   console.log(rhetoric[line]);
@@ -35,4 +38,4 @@ function infiniteRead() {
   setTimeout(infiniteRead, 5000);
 }
 
-infiniteRead();
+setTimeout(infiniteRead, 100);
