37d1e79809f668b48a0ef74de42e3173ce1a71de
Bret Zawilski <bret.zawilski@gmail.com>
2021-07-07 10:07:20 +0100

    Input and search options available

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const pageBody = document.getElementById("rhetoric");
+const pauseBtn = document.getElementById("pause-btn");
+const setLength = document.getElementById("set-length");
+const setLengthBtn = document.getElementById("set-length-btn");
+setLengthBtn.addEventListener("click", () => {
+  pageBody.innerHTML = "";
+  lineLength = Number(setLength.value);
+  setLength.value = null;
+  initialRead();
+});
+
+pauseBtn.addEventListener("click", () => {
+  if (!pause) {
+    pause = true;
+  } else {
+    pause = false;
+    infiniteRead();
+  }
+});
 
 let rhetoric = "";
+
+let pause = false;
+
+let lineLength = 4;
 
 let fetchRhetoric = fetch("rhetoric.json", {
   headers: {
@@ -18,11 +40,20 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+let saveText = () => {
+  let savedText;
+  let selectLines = document.querySelectorAll("p");
+  for (let i = 0; i < selectLines.length; i++) {
+    savedText += selectLines[i].textContent + " ";
+  }
+  return savedText.replace("undefined", "");
+};
+
 function printLine() {
   let line = getRandomInt(rhetoric.length);
   let addLine = document.createElement("p");
   let selectLines = document.querySelectorAll("p");
-  if (pageBody.childElementCount >= 3) {
+  if (pageBody.childElementCount >= lineLength) {
     selectLines[0].addEventListener("transitionend", function () {
       pageBody.firstElementChild.remove();
     });
@@ -35,6 +66,9 @@ function printLine() {
 }
 
 function infiniteRead() {
+  if (pause === true) {
+    return;
+  }
   if (rhetoric) {
     printLine();
   } else {
@@ -43,4 +77,11 @@ function infiniteRead() {
   setTimeout(infiniteRead, 5000);
 }
 
-setTimeout(infiniteRead, 100);
+function initialRead() {
+  for (let i = 0; i < lineLength; i++) {
+    printLine();
+  }
+}
+
+setTimeout(initialRead, 100);
+setTimeout(infiniteRead, 5000);
