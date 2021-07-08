35ee1ab30eb3a6731405447e0cb85b34d5159821
Bret Zawilski <bret.zawilski@gmail.com>
2021-07-08 14:30:51 +0100

    Refactored functions, reorganized code

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-const pageBody = document.getElementById("rhetoric");
-const pauseBtn = document.getElementById("pause-btn");
-const setLength = document.getElementById("set-length");
-const setLengthBtn = document.getElementById("set-length-btn");
-setLengthBtn.addEventListener("click", () => {
-  pageBody.innerHTML = "";
-  lineLength = Number(setLength.value);
-  // setLength.value = 1;
-  initialRead();
-});
-const modal = document.getElementById("modal");
-// modal.addEventListener("click", () => {
-//   modal.classList.toggle("hidden");
-// });
-const textArea = document.getElementById("text-area");
-const saveBtn = document.getElementById("save-btn");
-saveBtn.addEventListener("click", () => {
-  pause = true;
-  pauseBtn.innerText = "Resume";
-  modal.classList.toggle("hidden");
-  textArea.innerText = saveText();
-});
-
-const closeBtn = document.getElementById("close-btn");
-closeBtn.addEventListener("click", () => {
-  modal.classList.toggle("hidden");
-});
-
-pauseBtn.addEventListener("click", () => {
-  if (!pause) {
-    pause = true;
-    pauseBtn.innerText = "Resume";
-  } else {
-    pause = false;
-    pauseBtn.innerText = "Pause";
-    infiniteRead();
-  }
-});
-
 let rhetoric = "";
-
 let pause = false;
-
 let lineLength = 4;
 
-let fetchRhetoric = fetch("rhetoric.json", {
+fetch("rhetoric.json", {
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
@@ -52,40 +11,15 @@ let fetchRhetoric = fetch("rhetoric.json", {
   .then((response) => response.json())
   .then((data) => {
     rhetoric = data.rhetoric;
-    console.log(data.rhetoric[0]);
+    console.log(rhetoric[0]);
+    initialRead();
   });
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+setTimeout(infiniteRead, 5000);
 
-let saveText = () => {
-  let savedText;
-  let selectLines = document.querySelectorAll("p");
-  for (let i = 0; i < selectLines.length; i++) {
-    savedText += selectLines[i].textContent + " ";
-  }
-  return savedText.replace("undefined", "");
-};
-
-function printLine() {
-  let line = getRandomInt(rhetoric.length);
-  let addLine = document.createElement("p");
-  let selectLines = document.querySelectorAll("p");
-  if (pageBody.childElementCount >= lineLength) {
-    selectLines[0].addEventListener("transitionend", function () {
-      pageBody.firstElementChild.remove();
-    });
-    selectLines[0].classList.add("scrollOut");
-    setTimeout(addBlock, 500);
-  } else {
-    addBlock();
-  }
-  function addBlock() {
-    addLine.innerText = rhetoric[line];
-    addLine.classList.add("fade-in");
-    pageBody.appendChild(addLine);
-    console.log(rhetoric[line]);
+function initialRead() {
+  for (let i = 0; i < lineLength; i++) {
+    printLine();
   }
 }
 
@@ -101,11 +35,78 @@ function infiniteRead() {
   setTimeout(infiniteRead, 5000);
 }
 
-function initialRead() {
-  for (let i = 0; i < lineLength; i++) {
-    printLine();
+function printLine() {
+  let line = getRandomInt(rhetoric.length);
+  let addLine = document.createElement("p");
+  let selectLines = document.querySelectorAll("p");
+  if (rhetDiv.childElementCount >= lineLength) {
+    selectLines[0].addEventListener("transitionend", function () {
+      rhetDiv.firstElementChild.remove();
+    });
+    selectLines[0].classList.add("scrollOut");
+    setTimeout(addBlock, 500);
+  } else {
+    addBlock();
+  }
+  function addBlock() {
+    addLine.innerText = rhetoric[line];
+    addLine.classList.add("fade-in");
+    rhetDiv.appendChild(addLine);
+    console.log(rhetoric[line]);
   }
 }
 
-setTimeout(initialRead, 1000);
-setTimeout(infiniteRead, 5000);
+function setLength() {
+  rhetDiv.innerHTML = "";
+  lineLength = Number(setLengthInput.value);
+  initialRead();
+}
+
+function toggleModal() {
+  modal.classList.toggle("hidden");
+}
+
+function saveText() {
+  pause = true;
+  pauseBtn.innerText = "Resume";
+  toggleModal();
+  textArea.innerText = concatText();
+}
+
+function pauseText() {
+  if (!pause) {
+    pause = true;
+    pauseBtn.innerText = "Resume";
+  } else {
+    pause = false;
+    pauseBtn.innerText = "Pause";
+    infiniteRead();
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function concatText() {
+  let savedText;
+  let selectLines = document.querySelectorAll("p");
+  for (let i = 0; i < selectLines.length; i++) {
+    savedText += selectLines[i].textContent + " ";
+  }
+  return savedText.replace("undefined", "");
+}
+
+const rhetDiv = document.getElementById("rhetoric");
+const modal = document.getElementById("modal");
+const textArea = document.getElementById("text-area");
+const setLengthInput = document.getElementById("set-length");
+
+const pauseBtn = document.getElementById("pause-btn");
+pauseBtn.addEventListener("click", pauseText);
+const setLengthBtn = document.getElementById("set-length-btn");
+setLengthBtn.addEventListener("click", setLength);
+const saveBtn = document.getElementById("save-btn");
+saveBtn.addEventListener("click", saveText);
+const closeBtn = document.getElementById("close-btn");
+closeBtn.addEventListener("click", toggleModal);
